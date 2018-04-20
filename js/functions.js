59b72a1d496ab2090415b8c7f6fd688581bf3921
abreeze12345 <abreezemail@gmail.com>
2018-04-20 09:35:17 +0800

    s

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,10 @@
-$(document).ready(function(){
-    $(".about-card-1").flip
+
+
+//Initialize AnimateOnScroll
+$(document).ready(function () {
+    AOS.init();
 });
+
+
+//UFO animation
+$(".navbar-row").hide();
