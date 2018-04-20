82b5a72dea4848ad94ecca852cd9f354c66fd8c7
abreeze12345 <abreezemail@gmail.com>
2018-04-21 05:15:40 +0800

    s

--- js/functions.js
+++ js/functions.js
@@ -2,13 +2,9 @@
 $(document).ready(function () {
     AOS.init();
 });
-$(document).ready(function () {
-    if ($('body').hasClass('mobile')) {
-        var viewportHeight = $(".ufo-row").outerHeight();
-        $(".ufo-row").css({height: viewportHeight});
-    }
-});
 
 
 //UFO animation
-$(".navbar-row").hide();
+$(window).on("load", function() {
+    $("#ufo").
+});
