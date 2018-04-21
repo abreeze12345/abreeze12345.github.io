bd0f4749f0f21570c9d10f9aaacd515fb5aeae23
abreeze12345 <abreezemail@gmail.com>
2018-04-21 12:54:09 -0400

    animation changes

--- js/functions.js
+++ js/functions.js
@@ -5,10 +5,17 @@ $(document).ready(function () {
     });
 });
 
+//Andrew Header Animation
 $(window).on("load", function() {
     $(".hidden").removeClass("hidden");
     $(".hidden").addClass("animated");
     $("#name").addClass("fadeIn");
     $("#top-line").addClass("fadeIn");
     $("#bottom-line").addClass("fadeIn");
+    $("#left-nav").delay(3000).addClass("slideInLeft");
+    $("#right-nav").delay(3000).addClass("slideInRight");
 });
+
+$(window).on("load", function() {
+    $("")
+})
